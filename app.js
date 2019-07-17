d079dbdb1b7c9f1d1644530289cbd8e137f6889c
Ryogo Okura <nem1442so@gmail.com>
2019-07-17 11:59:25 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const Discord=require('discord.js');
+const client=new Discord.Client();
+client.on('ready',()=>{
+  console.log('ready...');
+});
+client.on('message',message=>{
+  if(message.author.bot){
+    return;
+  }else{
+    let msg=message.content;
+    let channel=message.channel;
+    const regexp=/^dice!(\d+)d(\d+)$/;
+    // const regexp=new RegExp('^dice!(\d+)d(\d+)$');
+    matchResult=msg.match(regexp);
+    if(matchResult){
+      const DiceNum=matchResult[1];
+      const DiceMax=matchResult[2];
+      console.log(`${DiceNum}d${DiceMax}`);
+      let txt=msg;
+      for(let i=0;i<DiceNum;i++){
+        txt+='\nnum: '+(Math.floor(Math.random()*DiceMax)+1)
+      }
+      message.reply(txt)
+        .then(message=>console.log(`message: ${msg}`))
+        .catch(console.error);
+    }
+    return;
+  }
+});
+
+client.login(process.env.TOKEN);
